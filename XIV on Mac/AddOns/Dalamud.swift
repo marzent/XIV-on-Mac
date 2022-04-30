c166e87f4a308eb2c7248d9e26483f65109c50ac
Zoey Cluff <matada89@gmail.com>
2022-04-30 03:14:35 -0500

    fixed spacing in previous commit

--- XIV on Mac/AddOns/Dalamud.swift
+++ XIV on Mac/AddOns/Dalamud.swift
@@ -196,7 +196,7 @@ struct Dalamud {
         }
     }
     
-      private static func ensureInstall() {
+    private static func ensureInstall() {
         guard let version = Remote.version else {
             print("Could not check for Dalamud due to network error\n", to: &Util.logger)
             return
