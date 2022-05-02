82f50433e9a0387a722fa24759d9832a087da610
marzent <marc_aurel@me.com>
2022-05-02 19:14:49 +0200

    bump SE base archive version

--- XIV on Mac/InstallerController.swift
+++ XIV on Mac/InstallerController.swift
@@ -184,7 +184,7 @@ class InstallerController: NSViewController {
     
     func install() {
         DispatchQueue.global(qos: .userInitiated).async { [self] in
-            let version = "1.0.5"
+            let version = "1.0.7"
             let url = URL(string: "https://mac-dl.ffxiv.com/cw/finalfantasyxiv-\(version).zip")!
             do {
                 try HTTPClient.fetchFile(url: url) { total, now, _ in
