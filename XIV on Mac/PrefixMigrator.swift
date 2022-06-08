7df361026bc24b6175463d869d9a55ae9792b7a8
marzent <marc_aurel@me.com>
2022-06-08 11:21:49 +0200

    move migration registry changes off the main thread

--- XIV on Mac/PrefixMigrator.swift
+++ XIV on Mac/PrefixMigrator.swift
@@ -12,8 +12,7 @@ class PrefixMigrator {
     static func migratePrefixIfNeeded() -> Bool {
         // Do we need to do anything? If there's a "game" folder in our prefix, then we need to migrate.
         let oldGameDirectory : URL = Util.applicationSupport.appendingPathComponent("game/", isDirectory: true)
-        if (!Util.pathExists(path: oldGameDirectory))
-        {
+        if (!Util.pathExists(path: oldGameDirectory)) {
             Log.information("Prefix Migration: No migration required.")
             return false
         }
@@ -21,8 +20,7 @@ class PrefixMigrator {
         Log.information("Prefix Migration: Existing prefix needs migration.")
         let oldPrefixPath : URL = Util.applicationSupport.appendingPathComponent("wineprefix_old/", isDirectory: true)
         let newPrefixPath : URL = Wine.prefix
-        if (!Util.pathExists(path: newPrefixPath))
-        {
+        if (!Util.pathExists(path: newPrefixPath)) {
             do {
                 try FileManager.default.createDirectory(atPath: newPrefixPath.path, withIntermediateDirectories: true, attributes: nil)
             }
@@ -52,70 +50,71 @@ class PrefixMigrator {
                 }
             }
         }
+
+        // Step 1: Move entire old prefix to backup location.
+        Log.information("Prefix Migration: Archiving existing prefix: \"\(oldGameDirectory.path)\" -> \"\(oldPrefixPath.path)\"")
         
         do {
-            // Step 1: Move entire old prefix to backup location.
-            Log.information("Prefix Migration: Archiving existing prefix: \"\(oldGameDirectory.path)\" -> \"\(oldPrefixPath.path)\"")
             try FileManager.default.moveItem(at: oldGameDirectory, to: oldPrefixPath)
+        } catch {
+            Log.error("Prefix Migration: Failed to move wine prefix: \(error.localizedDescription)")
+            return false
+        }
+        
+        // Now retrieve some stuff from the old prefix into the new.
+        let oldProgramFilesXIVOnMacPath = oldPrefixPath.appendingPathComponent("drive_c/Program Files/XIV on Mac/", isDirectory: true)
+        let itemsToRetrieve : [(URL,URL)] = [
+            // First we move the game data itself
+            (oldPrefixPath.appendingPathComponent("drive_c/Program Files (x86)/SquareEnix/FINAL FANTASY XIV - A Realm Reborn/", isDirectory: true),
+             Util.applicationSupport.appendingPathComponent("ffxiv/", isDirectory: true)
+            ),
             
-            // Now retrieve some stuff from the old prefix into the new.
-            let oldProgramFilesXIVOnMacPath = oldPrefixPath.appendingPathComponent("drive_c/Program Files/XIV on Mac/", isDirectory: true)
-            let itemsToRetrieve : [(URL,URL)] = [
-                // First we move the game data itself
-                (oldPrefixPath.appendingPathComponent("drive_c/Program Files (x86)/SquareEnix/FINAL FANTASY XIV - A Realm Reborn/", isDirectory: true),
-                 Util.applicationSupport.appendingPathComponent("ffxiv/", isDirectory: true)
-                ),
-                
-                // Dalamud stuff
-                (oldProgramFilesXIVOnMacPath.appendingPathComponent("devPlugins/", isDirectory: true),
-                 Util.applicationSupport.appendingPathComponent("devPlugins/", isDirectory: true)
-                ),
-                (oldProgramFilesXIVOnMacPath.appendingPathComponent("installedPlugins/", isDirectory: true),
-                 Util.applicationSupport.appendingPathComponent("installedPlugins/", isDirectory: true)
-                ),
-                (oldProgramFilesXIVOnMacPath.appendingPathComponent("pluginConfigs/", isDirectory: true),
-                 Util.applicationSupport.appendingPathComponent("pluginConfigs/", isDirectory: true)
-                ),
-                (oldProgramFilesXIVOnMacPath.appendingPathComponent("dalamudconfig.json", isDirectory: false),
-                 Util.applicationSupport.appendingPathComponent("dalamudconfig.json", isDirectory: false)
-                ),
+            // Dalamud stuff
+            (oldProgramFilesXIVOnMacPath.appendingPathComponent("devPlugins/", isDirectory: true),
+             Util.applicationSupport.appendingPathComponent("devPlugins/", isDirectory: true)
+            ),
+            (oldProgramFilesXIVOnMacPath.appendingPathComponent("installedPlugins/", isDirectory: true),
+             Util.applicationSupport.appendingPathComponent("installedPlugins/", isDirectory: true)
+            ),
+            (oldProgramFilesXIVOnMacPath.appendingPathComponent("pluginConfigs/", isDirectory: true),
+             Util.applicationSupport.appendingPathComponent("pluginConfigs/", isDirectory: true)
+            ),
+            (oldProgramFilesXIVOnMacPath.appendingPathComponent("dalamudconfig.json", isDirectory: false),
+             Util.applicationSupport.appendingPathComponent("dalamudconfig.json", isDirectory: false)
+            ),
 
-                // ACT stuff
-                (oldPrefixPath.appendingPathComponent("drive_c/users/emet-selch/Application Data/Advanced Combat Tracker/", isDirectory: true),
-                 newPrefixPath.appendingPathComponent("drive_c/users/emet-selch/Application Data/Advanced Combat Tracker/", isDirectory: true)
-                )
-                
-            ]
+            // ACT stuff
+            (oldPrefixPath.appendingPathComponent("drive_c/users/emet-selch/Application Data/Advanced Combat Tracker/", isDirectory: true),
+             newPrefixPath.appendingPathComponent("drive_c/users/emet-selch/Application Data/Advanced Combat Tracker/", isDirectory: true)
+            )
             
-            for oneItem in itemsToRetrieve {
-                let oldPath : URL = oneItem.0
-                let newPath : URL = oneItem.1
-                if (Util.pathExists(path: oldPath))
+        ]
+        
+        for oneItem in itemsToRetrieve {
+            let oldPath : URL = oneItem.0
+            let newPath : URL = oneItem.1
+            if (Util.pathExists(path: oldPath))
+            {
+                Log.information("Prefix Migration: Retrieving \"\(oldPath.path)\" -> \"\(newPath.path)\"")
+                // Get rid of an existing version at the destination
+                if (Util.pathExists(path: newPath))
                 {
-                    Log.information("Prefix Migration: Retrieving \"\(oldPath.path)\" -> \"\(newPath.path)\"")
-                    // Get rid of an existing version at the destination
-                    if (Util.pathExists(path: newPath))
-                    {
-                        // If removal fails we'll try to move anyway I guess...
-                        do {
-                            try FileManager.default.removeItem(at: newPath)
-                        }
-                        catch let removeError as NSError {
-                            Log.error("Prefix Migration: Failed to remove \(oldPath): \(removeError.localizedDescription)")
-                        }
-                    }
-                    // If this fails we should try the other stuff. Better to make a best effort than the block the user entirely.
+                    // If removal fails we'll try to move anyway I guess...
                     do {
-                        try FileManager.default.moveItem(at: oldPath, to: newPath)
+                        try FileManager.default.removeItem(at: newPath)
                     }
-                    catch let moveError as NSError {
-                        Log.error("Prefix Migration: Failed to retrieve \(oldPath.lastPathComponent): \(moveError.localizedDescription)")
+                    catch let removeError as NSError {
+                        Log.error("Prefix Migration: Failed to remove \(oldPath): \(removeError.localizedDescription)")
                     }
                 }
+                // If this fails we should try the other stuff. Better to make a best effort than the block the user entirely.
+                do {
+                    try FileManager.default.moveItem(at: oldPath, to: newPath)
+                }
+                catch let moveError as NSError {
+                    Log.error("Prefix Migration: Failed to retrieve \(oldPath.lastPathComponent): \(moveError.localizedDescription)")
+                }
             }
-        }
-        catch let migrationError as NSError {
-            Log.error("Prefix Migration: Failed to migrate: \(migrationError.localizedDescription)")
         }
         
         Log.information("Prefix Migration: Migration Complete.")
@@ -125,8 +124,10 @@ class PrefixMigrator {
     static func migrateWineRegistrySettings() {
         // And finally, let's assert that preferences backed by registry keys need to have their keys re-set to match our preference files.
         // Since the setters don't check that the value isn't changing, we can just set them to their current values to get the registry updated.
-        Wine.retina = Wine.retina
-        Settings.platform = Settings.platform
+        DispatchQueue.global(qos: .utility).async {
+            Wine.retina = Wine.retina
+            Settings.platform = Settings.platform
+        }
     }
 }
 
