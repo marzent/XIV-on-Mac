815774e970259e16f1b2d51b01e159b86b03da81
Marc-Aurel Zent <mzent@codeweavers.com>
2024-12-23 19:39:03 +0100

    bump standalone nix build deployment target

--- XIV on Mac/wine-builder/build.sh
+++ XIV on Mac/wine-builder/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-12.0}
+export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-13.5}
 ./nix-build.sh
 ./package-runtime.sh
